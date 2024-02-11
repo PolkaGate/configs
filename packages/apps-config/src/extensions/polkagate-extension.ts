23e8c8bed26937840010b32cea3c66413cfa39c7
Kami <ekbatanifard@gmail.com>
2024-02-11 18:56:09 +0330

    replace js extension with polkagate extension

--- packages/apps-config/src/extensions/polkagate-extension.ts
+++ packages/apps-config/src/extensions/polkagate-extension.ts
@@ -6,13 +6,13 @@ import type { Known } from './types.js';
 import { extensionsPolkadotJsSVG } from '../ui/logos/extensions/index.js';
 import { externalEmptySVG } from '../ui/logos/external/index.js';
 
-export const PolkadotJs: Known = {
+export const PolkagateExtension: Known = {
   all: {
-    chrome: 'https://chrome.google.com/webstore/detail/polkadot%7Bjs%7D-extension/mopnmbcafieddcagagdcbnhejhlodfdd',
-    firefox: 'https://addons.mozilla.org/en-US/firefox/addon/polkadot-js-extension/'
+    chrome: 'https://chromewebstore.google.com/detail/polkagate-the-gateway-to/ginchbkmljhldofnbjabmeophlhdldgp',
+    firefox: 'https://addons.mozilla.org/en-US/firefox/addon/polkagate/'
   },
-  desc: 'Basic account injection and signer',
-  name: 'polkadot-js extension',
+  desc: 'Polkagate extension/wallet',
+  name: 'polkagate extension',
   ui: {
     // Don't copy this line as-is :) The '|| empty' here is just there for a build
     // check, aka it actually has no effect (the first part is always defined)
