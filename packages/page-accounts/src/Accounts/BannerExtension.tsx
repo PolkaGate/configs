eb31cb61b4d2aebe1f27be16666efd4ffdf5d892
Kami <ekbatanifard@gmail.com>
2024-02-14 21:15:26 +0330

    add wallet urls

--- packages/page-accounts/src/Accounts/BannerExtension.tsx
+++ packages/page-accounts/src/Accounts/BannerExtension.tsx
@@ -82,7 +82,7 @@ function BannerExtension (): React.ReactElement | null {
       }</ul>
       <p>{t('Accounts injected from any of these extensions will appear in this application and be available for use. The above list is updated as more extensions with external signing capability become available.')}&nbsp;
         <a
-          href='https://github.com/PolkaGate/polkagate-extension'
+          href='https://polkadot.network/ecosystem/wallets/'
           rel='noopener noreferrer'
           target='_blank'
         >{t('Learn more...')}</a>
