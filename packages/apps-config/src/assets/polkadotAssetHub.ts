91ba1e1e8f08dce80d2585fa8ac6c715adac3a38
Kami <ekbatanifard@gmail.com>
2024-03-04 14:04:36 +0330

    add native asset

--- packages/apps-config/src/assets/polkadotAssetHub.ts
+++ packages/apps-config/src/assets/polkadotAssetHub.ts
@@ -1,33 +1,51 @@
 // Copyright 2017-2024 @polkadot/apps-config authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
+/* eslint-disable sort-keys */
+
 import type { Asset } from './types.js';
 
 import { assetsTetherUsdtSVG, assetsUsdCoinUsdcSVG } from '../ui/logos/assets/index.js';
+import { chainsPolkadotCircleSVG } from '../ui/logos/chains/generated/polkadot-circleSVG.js';
 
 export const polkadotAssetHub: Asset[] = [
   {
-    decimal: 6,
-    homepage: 'https://tether.to',
-    id: 1984,
-    priceId: 'tether',
-    symbol: 'USDt',
-    text: 'USD Tether',
+    id: -1, // for native token
+    symbol: 'DOT',
+    decimal: 10,
+    priceId: 'polkadot',
     ui: {
-      color: '#1bde87',
-      logo: assetsTetherUsdtSVG
+      color: '#de0377',
+      logo: chainsPolkadotCircleSVG,
+    },
+    extras: {
+      isNative: true
     }
   },
   {
+    id: 1984,
+    symbol: 'USDt',
     decimal: 6,
-    homepage: 'https://www.circle.com',
+    priceId: 'tether',
+    homepage: 'https://tether.to',
+    text: 'USD Tether',
+    ui: {
+      color: '#1bde87',
+      logo: assetsTetherUsdtSVG,
+      subLogo: true
+    }
+  },
+  {
     id: 1337,
-    priceId: 'usd-coin',
     symbol: 'USDC',
+    decimal: 6,
+    priceId: 'usd-coin',
+    homepage: 'https://www.circle.com',
     text: 'USD Coin',
     ui: {
       color: '#370afd',
-      logo: assetsUsdCoinUsdcSVG
+      logo: assetsUsdCoinUsdcSVG,
+      subLogo: true
     }
   }
 ];
