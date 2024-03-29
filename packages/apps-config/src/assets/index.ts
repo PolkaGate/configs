2227ba3fc574b5254438f7edac57607c33ec074a
Kami <ekbatanifard@gmail.com>
2024-03-29 14:00:00 +0330

    Update index.ts

--- packages/apps-config/src/assets/index.ts
+++ packages/apps-config/src/assets/index.ts
@@ -8,7 +8,7 @@ import { hydraDX } from './hydraDX.js';
 import { kusamaAssetHub } from './kusamaAssetHub.js';
 import { polkadotAssetHub } from './polkadotAssetHub.js';
 
-export function createAssets (): Record<string, Asset[]> | undefined {
+export function createAssets (): Record<string, Asset[]> {
   return {
     acala,
     hydraDX,
