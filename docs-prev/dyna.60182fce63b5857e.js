df1f3b7ce8701c6bede3876663b4a56741eb28de
Kami <ekbatanifard@gmail.com>
2024-07-04 15:27:02 +0330

    rm docs prev

--- docs-prev/dyna.60182fce63b5857e.js
+++
@@ -1,1 +0,0 @@
-"use strict";(("undefined"!=typeof self?self:this).webpackChunk_polkadot_apps=("undefined"!=typeof self?self:this).webpackChunk_polkadot_apps||[]).push([[8570],{68570:(e,p,s)=>{s.r(p)}}]);
