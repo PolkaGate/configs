60b429eddb812dc57e5bed587ab53edac5cc4f08
Kami <ekbatanifard@gmail.com>
2025-08-06 14:23:12 +0330

    fix: add test net files to the list

--- packages/apps-config/src/assets/index.ts
+++ packages/apps-config/src/assets/index.ts
@@ -6,13 +6,17 @@ import type { Asset } from './types.js';
 import { acala } from './acala.js';
 import { hydration } from './hydration.js';
 import { kusamaAssetHub } from './kusamaAssetHub.js';
+import { paseoAssetHub } from './paseoAssetHub.js';
 import { polkadotAssetHub } from './polkadotAssetHub.js';
+import { westendAssetHub } from './westendAssetHub.js';
 
 export function createAssets (): Record<string, Asset[]> {
   return {
     acala,
     hydration,
     kusamaAssetHub,
-    polkadotAssetHub
+    paseoAssetHub,
+    polkadotAssetHub,
+    westendAssetHub
   };
 }
