85a240a6bcdbef0ed3cd3f20ae99cd1964ee4c8d
Kami <ekbatanifard@gmail.com>
2025-10-06 15:14:20 +0330

    chore: dissmiss some linting

--- eslint.config.js
+++ eslint.config.js
@@ -1,4 +1,4 @@
-// Copyright 2017-2025 @polkadot/apps authors & contributors
+// Copyright 2017-2025 @polkadot/apps and @polkagate/apps-config authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
 import baseConfig from '@polkadot/dev/config/eslint';
@@ -6,6 +6,7 @@ import baseConfig from '@polkadot/dev/config/eslint';
 export default [
   ...baseConfig,
   {
+    files: ['src/**/*.ts', 'src/**/*.tsx'],
     rules: {
       // add override for any (a metric ton of them, initial conversion)
       '@typescript-eslint/no-explicit-any': 'off',
